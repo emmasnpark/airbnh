f383e9c03909c169a9ae0f3fcbdb0f4829a7ff84
Emma <seoyoon7011@gmail.com>
2018-02-27 18:26:17 +1100

    added stripe

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
 gem 'devise', '~> 4.2'
+gem 'stripe'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def avartar_url(user)
+  def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
   end
